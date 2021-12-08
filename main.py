7a206be0286f01005d585d3dec027a3b737ef86c
by-Tobd <86555139+by-Tobd@users.noreply.github.com>
2021-12-08 19:11:57 +0100

    Added files

---
+++ main.py
@@ -0,0 +1,65 @@
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator, refresh_access_token
+from twitchAPI.types import AuthScope
+from twitchAPI.pubsub import PubSub
+from elgato import Elgato, State, Info
+import os, toml, json, asyncio
+
+
+async def flashbang():
+    config = toml.load("config.toml")
+    async with Elgato(config.get("keylight")) as elgato:
+        before_state: State = await elgato.state()
+        await elgato.light(brightness=config.get("brightness"), on=True, temperature=((10**6)/config.get("temperature")))
+        asyncio.sleep(config.get("duration"))
+        await elgato.light(brightness=before_state.brightness, on=before_state.on, temperature=before_state.temperature)
+
+async def show_info():
+    config = toml.load("config.toml")
+    print(config.get("keylight"))
+    async with Elgato(config.get("keylight")) as elgato:
+        print(await elgato.info())
+        print(await elgato.state())
+    
+def on_event(uuid, data):
+    if data["type"] == "reward-redeemed":
+        id = data["data"]["redemption"]["reward"]["id"]
+        name = data["data"]["redemption"]["reward"]["title"]
+        config = toml.load("config.toml")
+        if config.get("reward_name") == name or config.get("reward_id") == id:
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(flashbang())
+
+
+if __name__ == "__main__":
+    if os.path.isfile("config.toml"):
+        config = toml.load("config.toml")
+        twitch = Twitch(config.get("client_id"), config.get("client_secret"))
+        scope = [AuthScope.CHANNEL_READ_REDEMPTIONS]
+        if not config.get("refresh_token"):
+            auth = UserAuthenticator(twitch, scope, force_verify=False)
+            token, refresh_token = auth.authenticate()
+        else:
+            token, refresh_token = refresh_access_token(config.get("refresh_token"), config.get("client_id"), config.get("client_secret"))
+        config.update({"refresh_token":refresh_token})
+        toml.dump(config, open("config.toml", "w"))
+        
+        twitch.set_user_authentication(token, scope, refresh_token)
+        user_id = twitch.get_users(logins=config.get("username"))["data"][0]["id"]
+        
+        pubsub = PubSub(twitch)
+        pubsub.start()
+        uuid = pubsub.listen_channel_points(user_id, on_event)
+        try:
+            while True:
+                msg = input("exit to close, info to show elgato info, test to flashbang ...")
+                if msg == "exit":
+                    break
+                elif msg == "info":
+                    loop = asyncio.get_event_loop()
+                    loop.run_until_complete(show_info())
+                elif msg == "test":
+                    loop = asyncio.get_event_loop()
+                    loop.run_until_complete(flashbang())
+        finally:
+            pubsub.stop()
